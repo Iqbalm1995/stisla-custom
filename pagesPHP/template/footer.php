b48de7b680ced12a323fc49ccf822dfe5854f5f1
iqbalm1995 <iqbalm1995@gmail.com>
2021-10-26 15:57:16 +0700

    init page php, view index v2

---
+++ pagesPHP/template/footer.php
@@ -0,0 +1,46 @@
+        <footer class="main-footer">
+            <div class="footer-left">
+            Copyright &copy; 2021 <div class="bullet"></div></a>
+            </div>
+            <div class="footer-right">
+            1.0.0
+            </div>
+        </footer>
+    </div>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+  <script src=".././assets/js/stisla.js"></script>
+  
+  <!-- JS Libraies -->
+  <script src=".././assets/modules/simple-weather/jquery.simpleWeather.min.js"></script>
+  <script src=".././assets/modules/chart.min.js"></script>
+  <script src=".././assets/modules/jqvmap/dist/jquery.vmap.min.js"></script>
+  <script src=".././assets/modules/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+  <script src=".././assets/modules/summernote/summernote-bs4.js"></script>
+  <script src=".././assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
+  <script src=".././assets/modules/bootstrap-daterangepicker/daterangepicker.js"></script>
+  <script src=".././assets/modules/bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
+  <script src=".././assets/modules/sweetalert/sweetalert.min.js"></script>
+  <script src=".././assets/modules/jquery-pwstrength/jquery.pwstrength.min.js"></script>
+  <script src=".././assets/modules/sticky-kit.js"></script>
+  <script src=".././assets/modules/datatables/datatables.min.js"></script>
+  <script src=".././assets/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js"></script>
+  <script src=".././assets/modules/datatables/Select-1.2.4/js/dataTables.select.min.js"></script>
+  <script src=".././assets/modules/jquery-ui/jquery-ui.min.js"></script>
+  
+  <!-- Page Specific JS File -->
+  <script src=".././assets/js/page/modules-sweetalert.js"></script>
+  <script src=".././assets/modules/bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
+  <script src=".././assets/modules/select2/dist/js/select2.full.min.js"></script>
+  <script src=".././assets/js/page/modules-datatables.js"></script>
+  
+  <!-- Template JS File -->
+  <script src=".././assets/js/scripts.js"></script>
+  <script src=".././assets/js/custom.js"></script>
+</body>
+</html>
---
+++ pagesPHP/template/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Ramon Dev &mdash; Stisla</title>
+
+  <!-- General CSS Files -->
+  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href=".././assets/modules/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href=".././assets/modules/datatables/datatables.min.css">
+  <link rel="stylesheet" href=".././assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href=".././assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css">
+
+  <link rel="stylesheet" href=".././assets/modules/bootstrap-daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href=".././assets/modules/jquery-selectric/selectric.css">
+  <link rel="stylesheet" href=".././assets/modules/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href=".././assets/modules/jqvmap/dist/jqvmap.min.css">
+  <link rel="stylesheet" href=".././assets/modules/weather-icon/css/weather-icons.min.css">
+  <link rel="stylesheet" href=".././assets/modules/weather-icon/css/weather-icons-wind.min.css">
+  <link rel="stylesheet" href=".././assets/modules/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href=".././assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css">
+  <link rel="stylesheet" href=".././assets/modules/select2/dist/css/select2.min.css">
+  <link rel="stylesheet" href=".././assets/modules/bootstrap-timepicker/css/bootstrap-timepicker.min.css">
+
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href=".././assets/css/stylev2.css">
+  <link rel="stylesheet" href=".././assets/css/components.css">
+
+  
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+</head>
+
+<body class="layout-2">
+  <div id="app">
+    <div class="main-wrapper">
+      <div class="navbar-bg"></div>
+      
+      <!-- Navbar & Sidebar Include-->
+      <?php 
+          include('navbar.php');
+          include('sidebar.php');
+      ?>
